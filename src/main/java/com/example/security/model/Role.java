275880842245366cec993eebc73a25859c6d9245
jean741 <iattiglah@gmail.com>
2021-09-05 01:33:47 +0200

    aa

--- src/main/java/com/example/security/model/Role.java
+++ src/main/java/com/example/security/model/Role.java
@@ -21,5 +21,11 @@ public class Role {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+	
 
 }
